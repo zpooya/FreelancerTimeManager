5a6b059730d786f9f5626d59e9d6162f5271bef6
zamzampooya <zamzampooya@gmail.com>
2018-10-18 20:00:13 +0330

    Grouping the project, Making views and viewControllers

---
+++ TimeManager/TimeManager/Controllers/AddTimeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddTimeViewController.swift
+//  TimeManager
+//
+//  Created by Zamzam Pooya on 10/18/18.
+//  Copyright © 2018 ZDevelop. All rights reserved.
+//
+
+import UIKit
+
+class AddTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
